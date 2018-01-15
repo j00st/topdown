a00f8b5c2e9e19f6b549810846687c2805b21ad1
Michelbaartman <Michelbaartman@users.noreply.github.com>
2018-01-15 13:50:55 +0100

    Test mousepos + playerpos

--- Code/TeamTopdown/TeamTopdown/TeamTopdown.cpp
+++ Code/TeamTopdown/TeamTopdown/TeamTopdown.cpp
@@ -10,21 +10,37 @@ using namespace sf;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	RenderWindow window(VideoMode(1200, 800), "SFML works!");
+	window.setFramerateLimit(60); //60 fps cinematic experience
+	//CircleShape shape(100.f);
+	RectangleShape mouseObject(Vector2f(20,20));
+	//RectangleShape line;
+	CircleShape player(10.f);
+	player.setFillColor(Color::Green);
+	mouseObject.setFillColor(Color::White);
+	Vector2f mousePos;
+	//Vector2f linePos(20, 20);
+	//shape.setFillColor(Color::Green);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
+		//update
+		player.setPosition(Vector2f(600, 400));
+		//line.setPosition(linePos);
+		mousePos = Vector2f(Mouse::getPosition(window));
+		mouseObject.setPosition(mousePos);
 
+		//draw
 		window.clear();
-		window.draw(shape);
+		window.draw(player);
+		window.draw(mouseObject);
+		//window.draw(line)
 		window.display();
 	}
 
