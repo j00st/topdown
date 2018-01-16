3135e216fc4a6b3d41a5eb5d1e43ad7a78d9472e
Michelbaartman <Michelbaartman@users.noreply.github.com>
2018-01-16 10:14:31 +0100

    naming + main loop

--- Code/TeamTopdown/TeamTopdown/mainController.cpp
+++ Code/TeamTopdown/TeamTopdown/mainController.cpp
@@ -9,14 +9,17 @@ using namespace sf;
 
 int main()
 {
+	// window setup
 	RenderWindow window(VideoMode(1200, 800), "SFML works!");
 	window.setFramerateLimit(60); //60 fps cinematic experience
+
+	// object setup
 	RectangleShape mouseObject(Vector2f(20,20));
-	//CircleShape player(10.f);
 	mouseObject.setFillColor(Color::White);
 	Vector2f mousePos;
 	Player player(Vector2f(20, 20));
 
+	// main loop
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -25,6 +28,7 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
+
 		//update
 		mousePos = Vector2f(Mouse::getPosition(window));
 		mousePos.x = mousePos.x - 10;
