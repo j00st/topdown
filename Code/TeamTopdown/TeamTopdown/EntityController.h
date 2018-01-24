e03ad2cb09e1e33c943e770b979405b77c7a5b3e
Michelbaartman <Michelbaartman@users.noreply.github.com>
2018-01-24 16:39:59 +0100

    comments

--- Code/TeamTopdown/TeamTopdown/EntityController.h
+++ Code/TeamTopdown/TeamTopdown/EntityController.h
@@ -25,21 +25,16 @@ private:
 	Player &player;
 	ControlsInput &ci;
 	Cursor &cursor;
-	Graphic background = Graphic("sprites/map0.png");
-	Graphic backgrounds = Graphic("sprites/map0s.png");
-	Map collisionMap = Map("sprites/map0c.png");
+	Graphic background = Graphic("sprites/map0.png"); /*!< background art */
+	Graphic backgrounds = Graphic("sprites/map0s.png"); /*!< background shadow map */
+	Map collisionMap = Map("sprites/map0c.png"); /*!< collision map */
 	std::vector<Entity*> entities = collisionMap.getEntities();
 	std::map< int, Bullet* > bullets{};
 	int bulletId = 0;
-	/*Entity* entities[2] = { 
-		new Crate(Vector2f(320.0f, 320.0f), Vector2f(64.0f, 64.0f)),
-		new Crate(Vector2f(160.0f, 160.0f), Vector2f(32.0f, 32.0f), false)
-	};*/
 
-	bool playerColliding(Vector2f direction);
-	bool checkBulletMap();
+	bool playerColliding(Vector2f direction); /*!< checks if player is colliding in direction */
+	bool checkBulletMap(); /*!< checks current bullets onscreen */
 public:
-	//basic hud//
 	RectangleShape staminaBar = RectangleShape(Vector2f(100, 10));
 	RectangleShape staminaBarBorder = RectangleShape(Vector2f(100, 10));
 	Font font;
@@ -49,12 +44,12 @@ public:
 
 
 	EntityController(Player &p, Cursor &c, ControlsInput &ci);
-	float calcSpeed();
-	void playerFire();
-	void update();
-	void updateHUD();
-	void drawHUD(RenderWindow& w);
-	void draw(RenderWindow & w);
+	float calcSpeed(); /*!< check how player is moving */
+	void playerFire(); /*!< check if player is firing */
+	void update(); /*!< updates current game */
+	void updateHUD(); /*!< Basic hud generation */
+	void drawHUD(RenderWindow& w); /*!< draws hud on screen */
+	void draw(RenderWindow & w); /*!< draws objects on screen */
 };
 
 #endif
--- Code/TeamTopdown/TeamTopdown/PlayerStats.h
+++ Code/TeamTopdown/TeamTopdown/PlayerStats.h
@@ -12,14 +12,14 @@ using namespace sf;
 \brief Struct that contains player statistics*/
 struct PlayerStats
 {
-	int stamina = 100;
-	float speed = 3;
-	bool dodging = false;
-	int ammo = 5;
-	int isDead = 0;
+	int stamina = 100; /*!< player max stamina */
+	float speed = 3; /*!< player max speed */
+	bool dodging = false; /*!< "is player dodging?" boolean */
+	int ammo = 5; /*!< current gun ammunition */
+	int isDead = 0; /*!< player dead state */
 
 	// ability cooldowns
-	Timer energy, sprint, dodge, shoot, reload;
+	Timer energy, sprint, dodge, shoot, reload; /*!< timer for player based on fps */
 
 	PlayerStats();
 };
