71ec17affdca13d64b62ea4bf874d83be96011e9
Dylrak <dylan.rakiman@student.hu.nl>
2018-01-26 18:08:17 +0100

    Added crate state functionality. Drops no items.

--- Code/TeamTopdown/TeamTopdown/Crate.cpp
+++ Code/TeamTopdown/TeamTopdown/Crate.cpp
@@ -5,13 +5,19 @@
 Crate::Crate(Vector2f position, Vector2f size, bool isSolid, int state):
 	Entity(position, size, isSolid, state)
 {
-	body.setPosition(position);
+	crate.setPosition(position);
 }
 
-void Crate::update() {
-	body.setPosition(position);
+void Crate::update(std::map<int, Bullet*> & bullets) {
+	for (auto bulletPair : bullets) {
+		if (collidesWith(bulletPair.second, Vector2f(0, 0))) {
+			state = states::destroyed;
+			crate.SetSprite(spriteStates[state]);
+			//drop item
+		}
+	}
 }
 
 void Crate::draw(RenderWindow & w) {
-	body.draw(w);
+	crate.draw(w);
 }
--- Code/TeamTopdown/TeamTopdown/Crate.h
+++ Code/TeamTopdown/TeamTopdown/Crate.h
@@ -2,6 +2,7 @@
 #define Crate_HEADER
 
 #include "Entity.h"
+#include "bullet.h"
 #include "Graphic.h"
 /*! \class Crate
 \brief Crate class
@@ -11,10 +12,12 @@ class Crate :
 	public Entity
 {
 private:
-	Graphic body = Graphic("sprites/crate.png");
+	enum states { normal, destroyed };
+	String spriteStates[2] = {"sprites/crate0.png" , "sprites/crate1.png" };
+	Graphic crate = Graphic(spriteStates[normal]);
 public:
-	Crate(Vector2f position, Vector2f size, bool isSolid = true, int state = 0);
-	void update() override;
+	Crate(Vector2f position, Vector2f size, bool isSolid = true, int state = states::normal);
+	void update(std::map<int, Bullet*> & bullets);
 	void draw(RenderWindow & w) override;
 };
 
