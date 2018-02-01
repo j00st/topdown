4f5783fe9f73bbf22b83bc3db04b2f42b576dbc1
Caedendi <bartcommandeur23@gmail.com>
2018-02-01 03:25:06 +0100

    removed old level states from solution

--- Code/TeamTopdown/TeamTopdown/AllGameStates.hpp
+++ Code/TeamTopdown/TeamTopdown/AllGameStates.hpp
@@ -7,9 +7,9 @@
 #include "CreditsState.hpp"
 #include "MainMenuState.hpp"
 #include "PlayingState.hpp"
-#include "Level1State.hpp"
-#include "Level2State.hpp"
-#include "Level3State.hpp"
-#include "Level4State.hpp"
+//#include "Level1State.hpp"
+//#include "Level2State.hpp"
+//#include "Level3State.hpp"
+//#include "Level4State.hpp"
 
 #endif // AllGameStates_HPP
